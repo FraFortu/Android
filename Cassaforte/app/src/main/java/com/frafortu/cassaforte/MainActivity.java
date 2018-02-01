1927a3b2f88ffc353a456e5542be548037ba5a71
FraFortu <francescof2800@gmail.com>
2018-02-01 22:23:35 +0100

    Cassaforte vers 1.2

--- Cassaforte/app/src/main/java/com/frafortu/cassaforte/MainActivity.java
+++ Cassaforte/app/src/main/java/com/frafortu/cassaforte/MainActivity.java
@@ -3,12 +3,12 @@ package com.frafortu.cassaforte;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private Button[] buttons;
     private int password = 1234;
 
     TextView display;
@@ -17,10 +17,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i = 0;
-        //buttons = new Button[11];
+        display = findViewById(R.id.text);
+    }
+    public boolean isPasswordCorrect(String text) {
+        if(text.isEmpty()) {
+            display.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }
+        if(Integer.parseInt(text) == password) {
+            display.setTextColor(getResources().getColor(R.color.green));
+            return true;
+        } else{
+            display.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }
+    }
 
-       /* buttons[i++] = findViewById(R.id.number0);
+    public void onClickListener(View event) {
+        if(event.getId() == R.id.number0) {
+            display.setText((display.getText() + "0"));
+        } else if(event.getId() ==  R.id.number1) {
+            display.setText((display.getText() + "1"));
+        } else if(event.getId() ==  R.id.number2) {
+            display.setText((display.getText() + "2"));
+        } else if(event.getId() ==  R.id.number3) {
+            display.setText((display.getText() + "3"));
+        } else if(event.getId() ==  R.id.number4) {
+            display.setText((display.getText() + "4"));
+        } else if(event.getId() ==  R.id.number5) {
+            display.setText((display.getText() + "5"));
+        } else if(event.getId() ==  R.id.number6) {
+            display.setText((display.getText() + "6"));
+        } else if(event.getId() ==  R.id.number7) {
+            display.setText((display.getText() + "7"));
+        } else if(event.getId() ==  R.id.number8) {
+            display.setText((display.getText() + "8"));
+        } else if(event.getId() ==  R.id.number9) {
+            display.setText((display.getText() + "9"));
+        } else if(event.getId() ==  R.id.delete) {
+            if(display.getText().length() > 1) {
+                display.setText(((String)display.getText()).substring(0, (display.getText().length() - 1)));
+            } else display.setText("");
+        } else if(event.getId() == R.id.ok) {
+            if(isPasswordCorrect((String) display.getText())) {
+                Intent intent = new Intent(this,MainActivity2.class);
+                intent.putExtra("password", password);
+                startActivityForResult(intent,2);
+            }
+        }
+        isPasswordCorrect((String) display.getText());
+    }
+    /*
+        Other version with array of buttons
+        Code to be put at onCreate method
+        private Button[] buttons;
+        int i = 0;
+        buttons[i++] = findViewById(R.id.number0);
         buttons[i++] = findViewById(R.id.number1);
         buttons[i++] = findViewById(R.id.number2);
         buttons[i++] = findViewById(R.id.number3);
@@ -30,49 +82,8 @@ public class MainActivity extends AppCompatActivity {
         buttons[i++] = findViewById(R.id.number7);
         buttons[i++] = findViewById(R.id.number8);
         buttons[i++] = findViewById(R.id.number9);
-        buttons[i++] = findViewById(R.id.delete);*/
-
-        display = findViewById(R.id.text);
-        Button b = new Button(this);
-        b.setOnClickListener(event -> {
-            if(event.getId() == R.id.number0) {
-                display.setText(((String) display.getText() + "0"));
-            } else if(event.getId() ==  R.id.number1) {
-                display.setText(((String) display.getText() + R.id.number1));
-            } else if(event.getId() ==  R.id.number2) {
-                display.setText(((String) display.getText() + R.id.number2));
-            } else if(event.getId() ==  R.id.number3) {
-                display.setText(((String) display.getText() + R.id.number3));
-            } else if(event.getId() ==  R.id.number4) {
-                display.setText(((String) display.getText() + R.id.number4));
-            } else if(event.getId() ==  R.id.number5) {
-                display.setText(((String) display.getText() + R.id.number5));
-            } else if(event.getId() ==  R.id.number6) {
-                display.setText(((String) display.getText() + R.id.number6));
-            } else if(event.getId() ==  R.id.number7) {
-                display.setText(((String) display.getText() + R.id.number7));
-            } else if(event.getId() ==  R.id.number8) {
-                display.setText(((String) display.getText() + R.id.number8));
-            } else if(event.getId() ==  R.id.number9) {
-                display.setText(((String) display.getText() + R.id.number9));
-            } else if(event.getId() ==  R.id.delete) {
-                display.setText(((String)display.getText()).substring(0, (display.getText().length() - 1)));
-            }
-            if(isPasswordCorrect((String) display.getText())) {
-                Intent intent = new Intent(this,MainActivity2.class);
-                intent.putExtra("password", password);
-                startActivityForResult(intent,2);
-            }
-        });
-
-        /*Button ok = findViewById(R.id.ok);
-        ok.setOnClickListener(event -> {
-            if(isPasswordCorrect((String) display.getText())) {
-                Intent intent = new Intent(this,MainActivity2.class);
-                intent.putExtra("password", password);
-                startActivityForResult(intent,2);
-            }
-        });
+        buttons[i++] = findViewById(R.id.delete);
+        buttons[i++] = findViewById(R.id.ok);
 
         for(i = 0; i < buttons.length - 1; i++) {
             int val = i;
@@ -87,14 +98,4 @@ public class MainActivity extends AppCompatActivity {
                 isPasswordCorrect((String) display.getText());
             } else display.setText("");
         });*/
-    }
-    public boolean isPasswordCorrect(String text) {
-        if(Integer.parseInt(text) == password) {
-            display.setTextColor(getResources().getColor(R.color.green));
-            return true;
-        } else{
-            display.setTextColor(getResources().getColor(R.color.red));
-            return false;
-        }
-    }
 }
